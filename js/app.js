5008be338105c3776a1905173a05d49806c6d58a
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-29 14:44:55 -0700

    added query selector in js for container, button, and img 1 2 3

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@
 
 console.log('Hello');
 
+let allItems = [];
+let myContainer = document.querySelector('section');
+let myButton =document.querySelector('div');
+let imageOne = document.querySelector('section img:first-child');
+let imageTwo = document.querySelector('section img:nth-child(2)');
+let imageThree = document.querySelector('section img:last-child');
+
+
+
+
