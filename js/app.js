b1e0b928dae392280b75f6f584a4b74053fb41e8
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-29 16:01:00 -0700

    while loop finished

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ new Item ('pen');
 new Item('pet-sweep');
 new Item('scissors');
 new Item('shark');
-new Item('sweep');
+new Item('sweep', 'png');
 new Item('tauntaun');
 new Item('unicorn');
 new Item('water-can');
@@ -50,3 +50,31 @@ new Item('wine-glass');
 // imageTwo.src = allItems[1].src;
 // imageOne.src = allItems[2].src;
 // allItems[0].views++;
+
+function selectRandomItemIndex(){
+  return Math.floor(Math.random() * allItems.length);
+}
+
+function renderRandomItems() {
+  let itemOne = selectRandomItemIndex();
+  let itemTwo = selectRandomItemIndex();
+  let itemThree =selectRandomItemIndex();
+  while (itemOne === itemTwo || itemThree === itemTwo)
+    itemTwo = selectRandomItemIndex();
+  while (itemThree === itemOne || itemTwo === itemOne)
+    itemOne = selectRandomItemIndex();
+  while( itemTwo === itemThree || itemOne === itemThree)
+    itemThree = selectRandomItemIndex();
+
+  console.log(`
+    Item one: ${itemOne}
+    Item two: ${itemTwo}
+    Item three: ${itemThree}
+  `);
+imageThree.src = allItems[itemThree].src;
+imageTwo.src = allItems[itemTwo].src;
+imageOne.src = allItems[itemOne].src;
+
+}
+
+renderRandomItems();
