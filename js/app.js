b0ad95de9eca9917c37ce52adad268c2410bf440
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-29 16:19:37 -0700

    added event handler to render new item when clicked on and for it to stop after 5 clicks. MUST CHANGE TO 25 CLICKS FOR SUBMISSION

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ console.log('Hello');
 
 let allItems = [];
 let myContainer = document.querySelector('section');
-let myButton =document.querySelector('div');
+let myButton = document.querySelector('div');
 let clicks = 0;
 let clicksAllowed = 5;
 
@@ -17,7 +17,7 @@ let imageThree = document.querySelector('section img:last-child');
 
 
 
-function Item (name, fileExtenstion = 'jpg') {
+function Item(name, fileExtenstion = 'jpg') {
   this.name = name;
   this.src = `img/${name}.${fileExtenstion}`;
   this.clicks = 0;
@@ -28,14 +28,14 @@ function Item (name, fileExtenstion = 'jpg') {
 new Item('bag');
 new Item('banana');
 new Item('bathroom');
-new Item ('boots');
+new Item('boots');
 new Item('breakfast');
 new Item('bubblegum');
 new Item('chair');
 new Item('cthulhu');
 new Item('dog-duck');
 new Item('dragon');
-new Item ('pen');
+new Item('pen');
 new Item('pet-sweep');
 new Item('scissors');
 new Item('shark');
@@ -51,30 +51,57 @@ new Item('wine-glass');
 // imageOne.src = allItems[2].src;
 // allItems[0].views++;
 
-function selectRandomItemIndex(){
+function selectRandomItemIndex() {
   return Math.floor(Math.random() * allItems.length);
 }
 
 function renderRandomItems() {
   let itemOne = selectRandomItemIndex();
   let itemTwo = selectRandomItemIndex();
-  let itemThree =selectRandomItemIndex();
+  let itemThree = selectRandomItemIndex();
   while (itemOne === itemTwo || itemThree === itemTwo)
     itemTwo = selectRandomItemIndex();
   while (itemThree === itemOne || itemTwo === itemOne)
     itemOne = selectRandomItemIndex();
-  while( itemTwo === itemThree || itemOne === itemThree)
+  while (itemTwo === itemThree || itemOne === itemThree)
     itemThree = selectRandomItemIndex();
 
-  console.log(`
-    Item one: ${itemOne}
-    Item two: ${itemTwo}
-    Item three: ${itemThree}
-  `);
-imageThree.src = allItems[itemThree].src;
-imageTwo.src = allItems[itemTwo].src;
-imageOne.src = allItems[itemOne].src;
+  imageOne.src = allItems[itemOne].src;
+  imageOne.alt = allItems[itemOne].name;
+  allItems[itemOne].views++;
+
+  imageTwo.src = allItems[itemTwo].src;
+  imageTwo.alt = allItems[itemTwo].name;
+  allItems[itemTwo].views++;
+
+
+  imageThree.src = allItems[itemThree].src;
+  imageThree.alt = allItems[itemThree].name;
+  allItems[itemThree].views++;
+
+
 
 }
 
 renderRandomItems();
+
+function handleItemClick(event){
+  if(event.target === myContainer){
+    alert('Click on one of the images');
+  }
+
+  clicks++;
+  let clickedItem = event.target.alt;
+  for (let i=0; i <allItems.length; i++){
+    if (clickedItem === allItems[i].name){
+      allItems[i].clicks++;
+    }
+  }
+  renderRandomItems();
+
+  if (clicks === clicksAllowed){
+    myContainer.removeEventListener('click', handleItemClick);
+  }
+}
+
+myContainer.addEventListener('click', handleItemClick);
