6c214046a7d2a25df29fa2714b207df7de0a43c2
jamiguel23 <josematthewmiguel@gmail.com>
2021-06-02 21:09:55 -0700

    added chart

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let allItems = [];
 let myContainer = document.querySelector('section');
 let myButton = document.querySelector('div');
 let clicks = 0;
-let clicksAllowed = 25;
+let clicksAllowed = 5;
 
 let imageOne = document.querySelector('section img:first-child');
 let imageTwo = document.querySelector('section img:nth-child(2)');
@@ -44,19 +44,14 @@ new Item('water-can');
 new Item('wine-glass');
 
 
-// imageThree.src = allItems[0].src;
-// imageTwo.src = allItems[1].src;
-// imageOne.src = allItems[2].src;
-// allItems[0].views++;
-
 function selectRandomItemIndex() {
   return Math.floor(Math.random() * allItems.length);
 }
 
 function renderRandomItems() {
-  while(renderListArray.length<3) {
+  while (renderListArray.length < 3) {
     let uniqueProduct = selectRandomItemIndex();
-    while(!renderListArray.includes(uniqueProduct)){
+    while (!renderListArray.includes(uniqueProduct)) {
       renderListArray.push(uniqueProduct);
     }
   }
@@ -64,12 +59,6 @@ function renderRandomItems() {
   let itemOne = renderListArray.pop();
   let itemTwo = renderListArray.pop();
   let itemThree = renderListArray.pop();
-  // while (itemOne === itemTwo || itemThree === itemTwo)
-  //   itemTwo = selectRandomItemIndex();
-  // while (itemThree === itemOne || itemTwo === itemOne)
-  //   itemOne = selectRandomItemIndex();
-  // while (itemTwo === itemThree || itemOne === itemThree)
-  //   itemThree = selectRandomItemIndex();
 
   imageOne.src = allItems[itemOne].src;
   imageOne.alt = allItems[itemOne].name;
@@ -120,13 +109,55 @@ function renderResult() {
   }
 }
 
+function renderChart(){
+  let ctx = document.getElementById('myChart').getContext('2d');
+  console.log(ctx);
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+}
+renderChart();
+
 
 function handleButtonClick(event) {
   if (clicks === clicksAllowed) {
     renderResult();
   }
 
-  myButton.removeEventListener('click', handleButtonClick );
+  myButton.removeEventListener('click', handleButtonClick);
 }
 
 
